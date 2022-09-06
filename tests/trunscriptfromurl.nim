bbb699b447fb1cfa92b6cfbd9e60e5e3148f9ad6
Thomas Nelson <tanelso2@gmail.com>
2022-09-06 17:39:02 -0500

    Use main branch in trunscriptfromurl

--- tests/trunscriptfromurl.nim
+++ tests/trunscriptfromurl.nim
@@ -6,7 +6,7 @@ import
   strformat,
   strutils
 
-const branch = "runScriptFromUrl-fix"
+const branch = "main"
 const folderUrl = fmt"https://raw.githubusercontent.com/tanelso2/nim_utils/{branch}/tests/util/"
 const trueUrl = folderUrl & "true.sh"
 const falseUrl = folderUrl & "false.sh"
