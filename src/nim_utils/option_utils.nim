d63ac2f09fce25e506f43ab2e7e766f67bb901dd
Thomas Nelson <tanelso2@gmail.com>
2022-09-23 22:56:42 -0500

    starting work on option pattern matching macro

--- src/nim_utils/option_utils.nim
+++ src/nim_utils/option_utils.nim
@@ -1,9 +1,42 @@
 import
   std/options,
-  std/sugar
+  std/sugar,
+  std/macros
 
 proc wrapException*[T](f: () -> T): Option[T] =
   try:
     return some(f())
   except:
     return none(T)
+
+proc noneBranch(nn: NimNode): NimNode =
+  nn.expectKind nnkStmtList
+  for i in 0..<len(nn):
+    case.nn[i].kind:
+      of nnkCall:
+        let cmdName = nn[i][0].`$`
+        if cmdName == "none":
+          let body = nn[i][1]
+          body.expectKind nnkStmtList
+          return body
+
+
+
+macro match*[T](this: Option[T], body: untyped) =
+  echo body.treeRepr()
+  let nb = body.noneBranch()
+  let sb = body.someBranch()
+  quote do:
+    if `this`.isSome():
+      let `someVarName` = `this`.get()
+      `sb`
+    else:
+      `nb`
+
+when isMainModule:
+  let x = none(int)
+  x.match:
+    some x:
+     echo x+1
+    none:
+     echo "nothing"
