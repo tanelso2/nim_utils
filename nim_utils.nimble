af6dc91fe86754e5baedc23203d7beb1f6dcd710
Thomas Nelson <tanelso2@gmail.com>
2022-10-12 21:19:44 -0500

    Add prompts with defaults to cli.nim

--- nim_utils.nimble
+++ nim_utils.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.16"
+version       = "0.1.17"
 author        = "Thomas Nelson"
 description   = "A collection of functions I've written in other projects and want to reuse."
 license       = "Unlicense"
--- src/nim_utils/cli.nim
+++ src/nim_utils/cli.nim
@@ -4,22 +4,38 @@ import
   strformat,
   strutils
 
+proc prompt*(msg: string): string =
+  stdout.write(fmt"{msg}: ")
+  return stdin.readline().strip()
+
+
+proc promptWithDefault*(msg, default: string): string =
+  let msgWithDefault = fmt"{msg} (default: {default})"
+  let res = prompt msgWithDefault
+  if res == "":
+    default
+  else:
+    res
+
 proc yesNoPrompt*(
   msg: string,
   autoyes: bool = false,
   autono: bool = false
 ): bool =
-  stdout.write(fmt"{msg} [y/N]: ")
+  let yesNoMsg = fmt"{msg} [y/N]"
+  var default = ""
   if autoyes and autono:
     raise newException(IOError, "autoyes and autono are both true... don't know what to do")
   elif autoyes:
-    info "assuming yes"
-    return true
+    default = "y"
   elif autono:
-    info "assuming no"
-    return true
+    default = "n"
 
-  let res = stdin.readline().strip
+  var res: string
+  if default == "":
+    res = prompt yesNoMsg
+  else:
+    res = promptWithDefault(yesNoMsg, default)
   case res:
     of "y", "Y":
       true
